732c9ce5e251f8ec486f51809ccd39e48b6635d4
Yang Liu <liuyang12@focusmedia.cn>
2019-12-13 10:03:34 +0800

    update

--- train.py
+++ train.py
@@ -8,15 +8,9 @@ epochs = 10
 lr = 1e-3
 device = 'cuda:0'
 normpc = kal.transforms.NormalizePointCloud()
-data = ModelNet('data/ModelNet10',
-                categories=['bed', 'bathtub'],
-                split='train', rep='pointcloud',
-                transform=normpc, device=device)
+data = ModelNet('data/ModelNet10', categories=['bed', 'bathtub'], split='train', transform=normpc, device=device)
 loader = DataLoader(data, batch_size=12, shuffle=True)
-val_data = ModelNet('data/ModelNet10',
-                    categories=['bed', 'bathtub'],
-                    split='test', rep='pointcloud',
-                    transform=normpc, device=device)
+val_data = ModelNet('data/ModelNet10', categories=['bed', 'bathtub'], split='test', transform=normpc, device=device)
 val_loader = DataLoader(val_data, batch_size=10, shuffle=False)
 model = kal.models.PointNet.PointNetClassifier(num_classes=2).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
