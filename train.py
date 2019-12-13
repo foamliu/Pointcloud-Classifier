a7cf571c077b8f26ba2fa3cb063c7b8b3099ac7e
Yang Liu <liuyang12@focusmedia.cn>
2019-12-13 10:06:10 +0800

    update

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import kaolin as kal
 import torch
-from kaolin.datasets.modelnet import ModelNet
+from kaolin.datasets.modelnet import ModelNetVoxels
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -8,9 +8,9 @@ epochs = 10
 lr = 1e-3
 device = 'cuda:0'
 normpc = kal.transforms.NormalizePointCloud()
-data = ModelNet('data/ModelNet10', categories=['bed', 'bathtub'], split='train', transform=normpc, device=device)
+data = ModelNetVoxels('data/ModelNet10', categories=['bed', 'bathtub'], split='train', transform=normpc, device=device)
 loader = DataLoader(data, batch_size=12, shuffle=True)
-val_data = ModelNet('data/ModelNet10', categories=['bed', 'bathtub'], split='test', transform=normpc, device=device)
+val_data = ModelNetVoxels('data/ModelNet10', categories=['bed', 'bathtub'], split='test', transform=normpc, device=device)
 val_loader = DataLoader(val_data, batch_size=10, shuffle=False)
 model = kal.models.PointNet.PointNetClassifier(num_classes=2).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
